f49d8181d88152de4847f36ec554a056e9eb73c5
nvhai142 <61268096+nvhai142@users.noreply.github.com>
2020-06-26 17:19:51 +0700

    Update LPMessagingSDKPlugin.swift

--- src/ios/LPMessagingSDKPlugin.swift
+++ src/ios/LPMessagingSDKPlugin.swift
@@ -455,7 +455,7 @@ extension String {
 
 
 //            LPMessagingSDK.instance.showConversation(self.conversationQuery!,authenticationCode: authenticationCode)
-            let conversationViewParams = LPConversationViewParams(conversationQuery: self.conversationQuery!, containerViewController: controller, isViewOnly: false)
+            let conversationViewParams = LPConversationViewParams(conversationQuery: self.conversationQuery!, containerViewController: nil, isViewOnly: false)
             let authenticationParams = LPAuthenticationParams(authenticationCode: nil, jwt: authenticationCode, redirectURI: nil)
             LPMessagingSDK.instance.showConversation(conversationViewParams, authenticationParams: nil)
        }
