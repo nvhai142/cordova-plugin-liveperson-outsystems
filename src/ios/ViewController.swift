eeb101d42ff12abac0833090b323e91a45e3bc98
nvhai142 <61268096+nvhai142@users.noreply.github.com>
2020-06-30 15:51:33 +0700

    Create ViewController.swift

---
+++ src/ios/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  LinhTinhSwift
+//
+//  Created by Hoan Nguyen on 3/23/20.
+//  Copyright © 2020 Hoan Nguyen. All rights reserved.
+//
+
+import UIKit
+import LPMessagingSDK
+import LPInfra
+import LPAMS
+
+class ViewController: UIViewController {
+
+    var conversationQuery:ConversationParamProtocol?;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.conversationQuery = LPMessagingSDK.instance.getConversationBrandQuery("2022139")
+        self.configUI()
+    }
+
+    
+    
+    func configUI() {
+        self.navigationController?.navigationBar.tintColor = UIColor.black
+        self.navigationController?.navigationBar.barTintColor = UIColor.white
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.title = "CHAT"
+        
+    }
+    
+    @IBAction func cancelPressed(sender:Any) {
+        print("cancel")
+    }
+    
+    @IBAction func optionPressed(sender:Any) {
+        if let query = self.conversationQuery {
+            let isChatActive = LPMessagingSDK.instance.checkActiveConversation(query)
+            
+            func showResolveConfirmation(title:String, message:String){
+                let confirmAlert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                confirmAlert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (alertAction) in
+                    LPMessagingSDK.instance.resolveConversation(query)
+                }))
+                confirmAlert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: nil))
+                self.present(confirmAlert, animated: true, completion: nil)
+            }
+            
+            func showClearConfirmation(){
+                let clearAlert = UIAlertController(title: "Clear history", message: "All of your existing conversation history will be lost. Are you sure?", preferredStyle: .alert)
+                clearAlert.addAction(UIAlertAction(title: "CLEAR", style: .default, handler: { (alertAction) in
+                    if isChatActive{
+                        showResolveConfirmation(title: "Clear history", message: "Please resolve the conversation first")
+                    }else {
+                       try? LPMessagingSDK.instance.clearHistory(query)
+                    }
+                }))
+                clearAlert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: nil))
+                self.present(clearAlert, animated: true, completion: nil)
+            }
+            
+            
+            
+            let alertVC = UIAlertController(title: "Menu", message: "Chooose an option", preferredStyle: .actionSheet)
+            
+            
+            let resolveAction = UIAlertAction(title: "Resolve the conversation", style: .default) { (alertAction) in
+                showResolveConfirmation(title: "Resolve the conversation", message: "Are you sure this topic is resolved?")
+            }
+            
+            let clearHistoryAction = UIAlertAction(title: "Clear history", style: .default) { (alertAction) in
+                showClearConfirmation()
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            
+            alertVC.addAction(resolveAction)
+            alertVC.addAction(clearHistoryAction)
+            alertVC.addAction(cancelAction)
+            
+            resolveAction.isEnabled = isChatActive;
+            self.present(alertVC, animated: true, completion: nil)
+        }
+    }
+}
