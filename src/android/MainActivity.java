55aa689b2c284b09d66ffa1bfa2e10facae37931
nvhai142 <61268096+nvhai142@users.noreply.github.com>
2020-07-10 10:05:38 +0700

    Update MainActivity.java

--- src/android/MainActivity.java
+++ src/android/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.liveperson.api.LivePersonCallbackImpl;
 import com.liveperson.api.sdk.LPConversationData;
@@ -31,6 +32,17 @@ public class MainActivity extends Activity {
         String package_name = getApplication().getPackageName();
         getMenuInflater().inflate(getApplication().getResources().getIdentifier("menu_chat", "menu", package_name), menu);
         mMenu = menu;
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
         return true;
     }
 }
