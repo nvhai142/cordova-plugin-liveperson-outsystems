f0a13a03608d37046f2fbf9bd67868e7d36eb5d3
nvhai142 <61268096+nvhai142@users.noreply.github.com>
2020-09-09 11:40:14 +0700

    Update ChatActivity.java

--- src/android/ChatActivity.java
+++ src/android/ChatActivity.java
@@ -141,7 +141,7 @@ public class ChatActivity extends AppCompatActivity implements SwipeBackLayout.S
     }
 
 
-    private void initFragment() {
+    private void initFragment(CampaignInfo campaignInfo) {
         mConversationFragment = (ConversationFragment)getSupportFragmentManager().findFragmentByTag(LIVEPERSON_FRAGMENT);
         Log.d(TAG, "initFragment. mConversationFragment = " + mConversationFragment);
         if (mConversationFragment == null) {
@@ -160,14 +160,10 @@ public class ChatActivity extends AppCompatActivity implements SwipeBackLayout.S
             ConversationViewParams conversationViewParams = new ConversationViewParams(false);
 
            
-            try {
-                // Create Campaign Object
-                campaign = new CampaignInfo(Long.valueOf(1244787870), Long.valueOf(1246064870),"", "", "");
-                // Log
-            } catch (BadArgumentException e){
-                // Log Error
+            if(campaignInfo!=null){
+                conversationViewParams.setCampaignInfo(campaignInfo);
             }
-            conversationViewParams.setCampaignInfo(campaign);
+            conversationViewParams.setCampaignInfo(campaignInfo);
 
             authParams.setHostAppJWT(authCode);
             //authParams.addCertificatePinningKey(publicKey);
@@ -194,29 +190,42 @@ public class ChatActivity extends AppCompatActivity implements SwipeBackLayout.S
         Bundle extras = getIntent().getExtras();
         String engagementAtt = "";
         if(extras != null) {
-            partyID= extras.getString("EXTRA_PARTYID");
             engagementAtt= extras.getString("EXTRA_ENGAGEMENT");
         }    
 
-        // Create Entry Points JSON
-        JSONArray entryPoints = null;
-        try {
-            entryPoints = new JSONArray("[\"http://www.liveperson-test.com\",\n" +
-                    "                   \"sec://visa-dev\",\n" +
-                    "                   \"lang://Eng\"]");
-        } catch (JSONException e) {
-            Log.e(TAG, "Error Creating Entry Points :: " + e);
-        }
-
-        // Create Engagement Attributes
         JSONArray engagementAttributes = null;
         try {
-            engagementAttributes = new JSONArray(engagementAtt);
+            engagementAttributes = new JSONArray("[\n" +
+                    "        {\n" +
+                    "        \"type\": \"personal\",\n" +
+                    "        \"personal\": {\n" +
+                    "        \"language\": \"en-UK\",\n" +
+                    "        \"contacts\": [\n" +
+                    "        {\n" +
+                    "        \"address\": {\n" +
+                    "        \"country\": \"South Korea\",\n" +
+                    "        \"region\": \"South Korea\"\n" +
+                    "        }\n" +
+                    "        }\n" +
+                    "        ]\n" +
+                    "        }\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "        \"info\": {\n" +
+                    "        \"storeZipCode\": \"South Korea\",\n" +
+                    "        \"accountName\": \"VISA\",\n" +
+                    "        \"customerId\": \"123\",\n" +
+                    "        \"storeNumber\": \"en-US\",\n" +
+                    "        \"ctype\": \"Infinite/Signature/Platinum\"\n" +
+                    "        },\n" +
+                    "        \"type\": \"ctmrinfo\"\n" +
+                    "        }\n" +
+                    "        ]");
         } catch (JSONException e) {
             Log.e(TAG, "Error Creating Engagement Attr :: " + e);
         }
         MonitoringParams params = new MonitoringParams("PageId", entryPoints, engagementAttributes);
-        LPMonitoringIdentity identity = new  LPMonitoringIdentity(partyID,"");
+        LPMonitoringIdentity identity = new  LPMonitoringIdentity(null,"");
 
         // Get Engagement
         LivepersonMonitoring.getEngagement(getApplicationContext(), Arrays.asList(identity), params, new EngagementCallback() {
@@ -238,25 +247,21 @@ public class ChatActivity extends AppCompatActivity implements SwipeBackLayout.S
                     // Try-Catch Block
                     try {
                         // Create Campaign Object
-                        campaign = new CampaignInfo(Long.valueOf(currentCampaignId), Long.valueOf(currentEngagementId),
+                        CampaignInfo campaign = new CampaignInfo(Long.valueOf(currentCampaignId), Long.valueOf(currentEngagementId),
                                 currentEngagementContextId, currentSessionId, currentVisitorId);
-                        // Log
-                        Log.d(TAG, "Campaign :: " + campaign);
+                        initFragment(campaign);
                     } catch (BadArgumentException e){
-                        // Log Error
-                        Log.d(TAG, "Error Creating Campaign :: " + e.getLocalizedMessage());
+                        initFragment(null);
                     }
                 } else {
                     // Log Error
-                    Log.d(TAG, "No Engagement found");
+                    initFragment(null);
                 }
             }
 
             @Override
             public void onError(MonitoringErrorType monitoringErrorType, Exception e) {
-                // Log Error
-                Log.d(TAG, "Error Getting Engagement :: " + e);
-                Log.d(TAG, "Error Getting Engagement :: " + monitoringErrorType);
+                initFragment(null);
             }
         });
     }
@@ -330,15 +335,15 @@ public class ChatActivity extends AppCompatActivity implements SwipeBackLayout.S
         String newAPP;
         if(extras != null) {
             newAPP= extras.getString("EXTRA_APPID");
-    
-            LivePerson.initialize(getApplicationContext(), new InitLivePersonProperties(newAPP, AppID, new InitLivePersonCallBack() {
+            MonitoringInitParams monitoringParams = new MonitoringInitParams("443bc965-320f-402b-92ce-3a79cf831267");
+            LivePerson.initialize(getApplicationContext(), new InitLivePersonProperties(newAPP, AppID, monitoringParams, new InitLivePersonCallBack() {
 
                 @Override
                 public void onInitSucceed() {
                     Log.i("HAN_NGUYEN", "Liverperson SDK Initialized" + LivePerson.getSDKVersion());
                     setUserProfile();
                     initEngagementAttributes();
-                    initFragment();
+                   // initFragment();
                 }
     
                 @Override
