1fe979d77a1e479ad49fb630ecce8d3c23d47189
nvhai142 <61268096+nvhai142@users.noreply.github.com>
2020-09-09 16:31:04 +0700

    update

--- src/ios/ConversationVC.swift
+++ src/ios/ConversationVC.swift
@@ -10,10 +10,7 @@ import UIKit
 import LPMessagingSDK
 import LPInfra
 
-class ConversationVC: UIViewController {
-    
-    @IBOutlet weak var loadingView: UIView!
-    
+class ConversationVC: UIViewController, LPMessagingSDKdelegate {
     
     func LPMessagingSDKObseleteVersion(_ error: NSError) {
         
@@ -30,12 +27,6 @@ class ConversationVC: UIViewController {
     func LPMessagingSDKError(_ error: NSError) {
         
     }
-    func LPMessagingSDKConnectionStateChanged(_ isReady: Bool, brandID: String) {
-        DispatchQueue.main.async {
-            self.loadingView.isHidden = true
-            self.loadingView.removeFromSuperview()
-        }
-    }
     
     func LPMessagingSDKAgentDetails(_ agent: LPUser?) {
         if let user = agent{
@@ -47,28 +38,21 @@ class ConversationVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addLoadingIndicator()
         LPMessagingSDK.instance.delegate = self
         let campaignInfo = LPCampaignInfo(campaignId: 1244787870, engagementId: 1246064870, contextId: nil)
         self.conversationQuery = LPMessagingSDK.instance.getConversationBrandQuery("47817293", campaignInfo: campaignInfo)
         self.configUI()
     }
 
-    private func addLoadingIndicator(){
-        let indicator = UIActivityIndicatorView(style: .large)
-        indicator.color = UIColor.white
-        indicator.startAnimating()
-        indicator.center = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height/2)
-        self.loadingView.addSubview(indicator)
-    }
-
+    
+    
     func configUI() {
         self.navigationController?.navigationBar.tintColor = UIColor.white
         self.navigationController?.navigationBar.barTintColor = UIColor.userBubbleBackgroundColor
         self.navigationController?.navigationBar.isTranslucent = false
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white];
         self.title = "CHAT"
-
+        
         let configUI = LPConfig.defaultConfiguration
         configUI.userBubbleBackgroundColor = UIColor.userBubbleBackgroundColor
         configUI.userBubbleBorderColor = UIColor.userBubbleBorderColor
@@ -118,9 +102,9 @@ class ConversationVC: UIViewController {
         configUI.fileCellLoaderRingBackgroundColor = UIColor.fileCellLoaderRingBackgroundColor
         configUI.isReadReceiptTextMode = false
         configUI.checkmarkVisibility = .sentOnly
-        configUI.csatShowSurveyView = false
+        configUI.csatShowSurveyView = false 
     }
-
+    
     @IBAction func cancelPressed(sender:Any) {
         if self.conversationQuery != nil {
             LPMessagingSDK.instance.removeConversation(self.conversationQuery!)
@@ -135,7 +119,7 @@ class ConversationVC: UIViewController {
     @IBAction func optionPressed(sender:Any) {
         if let query = self.conversationQuery {
             let isChatActive = LPMessagingSDK.instance.checkActiveConversation(query)
-
+            
             func showResolveConfirmation(title:String, message:String){
                 let confirmAlert = UIAlertController(title: title, message: message, preferredStyle: .alert)
                 confirmAlert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (alertAction) in
@@ -144,7 +128,7 @@ class ConversationVC: UIViewController {
                 confirmAlert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: nil))
                 self.present(confirmAlert, animated: true, completion: nil)
             }
-
+            
             func showClearConfirmation(){
                 let clearAlert = UIAlertController(title: "Clear history", message: "All of your existing conversation history will be lost. Are you sure?", preferredStyle: .alert)
                 clearAlert.addAction(UIAlertAction(title: "CLEAR", style: .default, handler: { (alertAction) in
@@ -157,27 +141,27 @@ class ConversationVC: UIViewController {
                 clearAlert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: nil))
                 self.present(clearAlert, animated: true, completion: nil)
             }
-
+            
             let st = UIStoryboard()
             st.instantiateInitialViewController()
-
+            
             let alertVC = UIAlertController(title: "Menu", message: "Chooose an option", preferredStyle: .actionSheet)
-
-
+            
+            
             let resolveAction = UIAlertAction(title: "Resolve the conversation", style: .default) { (alertAction) in
                 showResolveConfirmation(title: "Resolve the conversation", message: "Are you sure this topic is resolved?")
             }
-
+            
             let clearHistoryAction = UIAlertAction(title: "Clear history", style: .default) { (alertAction) in
                 showClearConfirmation()
             }
-
+            
             let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-
+            
             alertVC.addAction(resolveAction)
             alertVC.addAction(clearHistoryAction)
             alertVC.addAction(cancelAction)
-
+            
             resolveAction.isEnabled = isChatActive;
             self.present(alertVC, animated: true, completion: nil)
         }
