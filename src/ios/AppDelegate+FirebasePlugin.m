81c5ca10e65064c7e247ff1310d4a97e584d2449
nvhai142 <61268096+nvhai142@users.noreply.github.com>
2020-10-08 16:44:50 +0700

    update

---
+++ src/ios/AppDelegate+FirebasePlugin.m
@@ -0,0 +1,46 @@
+#import "AppDelegate+FirebasePlugin.h"
+#import <objc/runtime.h>
+
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+  @import UserNotifications;
+
+  // Implement UNUserNotificationCenterDelegate to receive display notification via APNS for devices
+  // running iOS 10 and above. Implement FIRMessagingDelegate to receive data message via FCM for
+  // devices running iOS 10 and above.
+  @interface AppDelegate () <UNUserNotificationCenterDelegate>
+  @end
+#endif
+
+#define kApplicationInBackgroundKey @"applicationInBackground"
+#define kDelegateKey @"delegate"
+
+@implementation AppDelegate (FirebasePlugin)
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    NSLog(@"FirebasePlugin - Disconnected from FCM");
+}
+
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSLog(@"FirebasePlugin - deviceToken1 = %@", deviceToken);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+ 
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+    fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+  NSLog(@"FirebasePlugin - Unable to register for remote notifications: %@", error);
+}
+
+
+@end
