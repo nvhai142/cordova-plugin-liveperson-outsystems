a3a918b7c727d98c23091e90d41e2f68f2929446
nvhai142 <61268096+nvhai142@users.noreply.github.com>
2020-10-12 11:12:51 +0700

    Update AppDelegate+LPMessagingSDKPlugin.h

--- src/ios/AppDelegate+LPMessagingSDKPlugin.h
+++ src/ios/AppDelegate+LPMessagingSDKPlugin.h
@@ -7,6 +7,8 @@
 @interface AppDelegate (LPMessagingSDKPlugin)
 @property(nonatomic, strong) NSNumber *applicationInBackground;
 
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
+
 #if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
   @property(NS_NONATOMIC_IOSONLY, nullable, weak) id<UNUserNotificationCenterDelegate> delegate;
 #endif
