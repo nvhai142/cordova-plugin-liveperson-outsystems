5f01eca16259033d1c1ea51f4488df7f9f7bb6ea
nvhai142 <61268096+nvhai142@users.noreply.github.com>
2020-11-16 09:50:09 +0700

    Update ConversationVC.swift

--- src/ios/ConversationVC.swift
+++ src/ios/ConversationVC.swift
@@ -52,6 +52,19 @@ class ConversationVC: UIViewController, LPMessagingSDKdelegate {
     var conversationQuery:ConversationParamProtocol?;
     var alert = UIAlertController(title: nil, message: "Loading...", preferredStyle: .alert)
 
+    var WelcomeMsg:String ="How can I help you today?"
+    var ClearConversationMsg:String ="All of your existing conversation history will be lost. Are you sure?"
+    var ClearConfirmMsg:String ="Please resolve the conversation first."
+    var ChooseMsg:String ="Choose an option"
+    var RevolvedTileMsg:String ="Resolve the conversation"
+    var ResolvedConfirmMsg:String ="Are you sure this topic is resolved?"
+    var ClearTitleMsg:String ="Clear Conversation"
+    var YesMsg:String ="Yes"
+    var CancelMsg:String ="Cancel"
+    var ClearMsg:String ="Clear"
+    var MenuMsg:String ="Menu"
+    var ChatTitleHeader:String ="Visa Concierge"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         LPMessagingSDK.instance.delegate = self
@@ -75,7 +88,7 @@ class ConversationVC: UIViewController, LPMessagingSDKdelegate {
         self.navigationController?.navigationBar.barTintColor = UIColor.csatNavigationBackgroundColor
         self.navigationController?.navigationBar.isTranslucent = false
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white];
-        self.title = "Visa Concierge"
+        self.title = ChatTitleHeader
         self.view.backgroundColor = UIColor.conversationBackgroundColor
         
         let configUI = LPConfig.defaultConfiguration
@@ -153,41 +166,41 @@ class ConversationVC: UIViewController, LPMessagingSDKdelegate {
             
             func showResolveConfirmation(title:String, message:String){
                 let confirmAlert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-                confirmAlert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (alertAction) in
+                confirmAlert.addAction(UIAlertAction(title: YES, style: .default, handler: { (alertAction) in
                     LPMessagingSDK.instance.resolveConversation(query)
                 }))
-                confirmAlert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: nil))
+                confirmAlert.addAction(UIAlertAction(title: CancelMsg, style: .cancel, handler: nil))
                 self.present(confirmAlert, animated: true, completion: nil)
             }
             
             func showClearConfirmation(){
-                let clearAlert = UIAlertController(title: "Clear Conversation", message: "All of your existing conversation history will be lost. Are you sure?", preferredStyle: .alert)
-                clearAlert.addAction(UIAlertAction(title: "CLEAR", style: .default, handler: { (alertAction) in
+                let clearAlert = UIAlertController(title: ClearTitleMsg, message: ClearConversationMsg, preferredStyle: .alert)
+                clearAlert.addAction(UIAlertAction(title: ClearMsg, style: .default, handler: { (alertAction) in
                     if isChatActive{
-                        showResolveConfirmation(title: "Clear Conversation", message: "Please resolve the conversation first")
+                        showResolveConfirmation(title: ClearTitleMsg, message: ClearConfirmMsg)
                     }else {
                        try? LPMessagingSDK.instance.clearHistory(query)
                     }
                 }))
-                clearAlert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: nil))
+                clearAlert.addAction(UIAlertAction(title: CancelMsg, style: .cancel, handler: nil))
                 self.present(clearAlert, animated: true, completion: nil)
             }
             
             let st = UIStoryboard()
             st.instantiateInitialViewController()
             
-            let alertVC = UIAlertController(title: "Menu", message: "Choose an option", preferredStyle: .actionSheet)
+            let alertVC = UIAlertController(title: MenuMsg, message: ChooseMsg, preferredStyle: .actionSheet)
             
             
-            let resolveAction = UIAlertAction(title: "Resolve the conversation", style: .default) { (alertAction) in
-                showResolveConfirmation(title: "Resolve the conversation", message: "Are you sure this topic is resolved?")
+            let resolveAction = UIAlertAction(title: RevolvedTileMsg, style: .default) { (alertAction) in
+                showResolveConfirmation(title: RevolvedTileMsg, message: ResolvedConfirmMsg)
             }
             
-            let clearHistoryAction = UIAlertAction(title: "Clear Conversation", style: .default) { (alertAction) in
+            let clearHistoryAction = UIAlertAction(title: ClearTitleMsg, style: .default) { (alertAction) in
                 showClearConfirmation()
             }
             
-            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            let cancelAction = UIAlertAction(title: CancelMsg, style: .cancel, handler: nil)
             
             alertVC.addAction(resolveAction)
             alertVC.addAction(clearHistoryAction)
